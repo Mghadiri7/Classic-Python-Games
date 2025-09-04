1706d023c43b9ba92daab326db3a0131c2125179
Mohammad Ghadiri <ghmohammad777@gmail.com>
2025-09-04 04:35:04 -0400

    Add files via upload

---
+++ Tic_Tac_Toe/constants.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+WINDOW_SIZE = (400,400)
+GAME_NAME = "Tic-Tac-Toe"
+COLOR = (255,255,255)
+RED = (255,0,0)
+BLUE = (0,0,255)
+SIZE = 120
+HALF = SIZE//2
+OFFSET = 20
+
+Game_board = np.array([[0,0,0], [0,0,0], [0,0,0]])
