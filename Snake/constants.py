5aef3de59f23072ea3ce5f5d111a8c5f428e48b3
Mohammad Ghadiri <ghmohammad777@gmail.com>
2025-09-05 03:11:16 -0400

    Add files via upload

---
+++ Snake/constants.py
@@ -0,0 +1,12 @@
+WINDOW_SIZE = (400,400)
+GAME_NAME = "Snake"
+WHITE = (255,255,255)
+BLACK = (0,0,0)
+RED = (255,0,0)
+SPEED = 1
+DELAY = 10
+TILE_SIZE = 20
+W_T = WINDOW_SIZE[1]//TILE_SIZE
+snake = [[0, W_T//2],
+         [1, W_T//2],
+         [2, W_T//2]]
